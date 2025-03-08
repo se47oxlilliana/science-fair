b16b3c51ac19decd397e0cc9028c0366dae14f78
se47oxlilliana <se47oxlilliana@gmx.com>
2025-03-08 11:19:57 +0000

    Add 5.php

---
+++ src/5.php
@@ -0,0 +1,5 @@
+<?php
+$scientific_research = array("Animal Behavior", "Astrophysics", "Biochemistry", "Chemical Engineering", "Civil Engineering", "Computer Science");
+$random_index = rand(0,6);
+echo $scientific_research[$random_index];
+?>
